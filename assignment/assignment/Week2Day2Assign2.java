03c58c4253bb4915f2c7b02d89ed4f42a99c7ab8
chrisjoy1985 <129819953+chrisjoy1985@users.noreply.github.com>
2023-04-05 17:14:26 +0100

    Add files via upload

---
+++ assignment/assignment/Week2Day2Assign2.java
@@ -0,0 +1,39 @@
+package assignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Week2Day2Assign2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver driver=new ChromeDriver();
+		//driver.get("https://login.salesforce.com/");
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.manage().window().maximize();
+		// Thread.sleep(2000);
+		// driver.close();
+		
+		driver.findElement(By.id("username")).sendKeys("DemoCsr");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TATA Consultancy Services");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Chrissie");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Samraj");
+		//driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("cjoylin@gmail.com");
+		//driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("07438879499");
+		driver.findElement(By.className("smallSubmit")).click();
+		driver.findElement(By.linkText("Edit")).click();
+		driver.findElement(By.id("updateLeadForm_companyName")).clear();
+		driver.findElement(By.id("updateLeadForm_companyName")).sendKeys("CITI BANK");		
+		driver.findElement(By.className("smallSubmit")).click();
+
+	}
+
+}
